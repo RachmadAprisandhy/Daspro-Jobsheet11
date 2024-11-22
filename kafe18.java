a410ea4fad0fe3b4b2a4504ca6ea7293288a4c4c
Rachmad Aprisandy <rachmadblackspade212@gmail.com>
2024-11-22 22:45:43 +0700

    commit 3

--- kafe18.java
+++ kafe18.java
@@ -1,7 +1,10 @@
 public class kafe18 {
 
-
-    public static void main(String[] args) {
+    public static void menu(String NamaPelanggan, boolean isMember){
+        System.out.println("selamat datang pelanggan : " + NamaPelanggan + "!");
+        if (isMember){
+            System.out.println("Anda adalah member,anda mendapat diskon 10% di setiap pembelian");
+        }
         System.out.println("========== MENU RESTORAN DaVinci ==========");
         System.out.println("1. Kopi Hitam - 15000");
         System.out.println("2. Capuccino - 20000");
@@ -11,5 +14,10 @@ public class kafe18 {
         System.out.println("6. Mie goreng - 18000");
         System.out.println("===========================================");
         System.out.println(" =Silahkan masukan menu yang anda inginkan=");
+        
+    }
+
+    public static void main(String[] args) {
+        menu("Andi",true);
     }
 }
